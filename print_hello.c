18b902fafd0e616347970c6128f9d167fcb8ac9f
Manuayu <44335325+Manuayu@users.noreply.github.com>
2018-11-19 22:21:55 +0530

    Create print_hello.c

---
+++ print_hello.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    
+    if(n>0)
+    {
+        for(i=1;i<=n;i++)
+        {
+            printf("Hello\n");
+        }
+    }
+    else
+    {
+        printf("\n");
+    }
+}
