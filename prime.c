e97cd97dc36443d60754800732254c0e1365aadf
Manuayu <44335325+Manuayu@users.noreply.github.com>
2018-11-24 10:34:07 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int num,i,flag=0;
+    scanf("%d",&num);
+    
+    for(i=2;i <= num/2;i++)
+    {
+        if(num%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("no");
+    }
+    else
+    {
+        printf("yes");
+    }
+}
